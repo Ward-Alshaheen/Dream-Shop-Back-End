7258f5131b86e6eda9b8243a234caae4ffdbd417
Ward-Alshaheen <wardalshaheen1@gmaul.com>
2022-01-07 16:41:51 +0200

    laravel v5.6.9 edit Comment

--- app/Http/Controllers/Product/CommentController.php
+++ app/Http/Controllers/Product/CommentController.php
@@ -19,16 +19,21 @@ class CommentController extends Controller
     /**
      * @throws Exception
      */
-    public function all(int $id): JsonResponse
+    public function all(int $id, Request $request): JsonResponse
     {
-        $comments=Comment::with('user:id,email,bio,image,name')
-        ->where('product_id',$id)->get();
-        foreach ($comments as $comment){
-            $comment['time']=$this->timeAgo($comment['created_at']);
+        $comments = Comment::with('user:id,email,bio,image,name')
+            ->where('product_id', $id)->get();
+        foreach ($comments as $comment) {
+            if ($request->hasHeader("lang")) {
+                $comment['time'] = $this->timeAgo($comment['created_at'], $request->header('lang'));
+            } else {
+                $comment['time'] = $this->timeAgo($comment['created_at']);
+            }
         }
-        return $this->returnData('comments',$comments);
+        return $this->returnData('comments', $comments);
     }
-    public function add(Request $request,int $id): JsonResponse
+
+    public function add(Request $request, int $id): JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'text' => 'required|string',
@@ -36,30 +41,31 @@ class CommentController extends Controller
         if ($validator->fails()) {
             return $this->returnError(401, $validator->errors());
         }
-        if(!Product::find($id)){
-            return $this->returnError(401,"product not found");
+        if (!Product::find($id)) {
+            return $this->returnError(401, "product not found");
         }
         Comment::create([
-            "user_id"=>Auth::id(),
-            'text'=>$request->all()['text'],
-            "product_id"=>$id
+            "user_id" => Auth::id(),
+            'text' => $request->all()['text'],
+            "product_id" => $id
         ]);
         return $this->returnSuccessMessage("Successfully");
     }
+
     public function delete(int $id): JsonResponse
     {
-        $comment=Comment::find($id);
-        if (!$comment){
-           return $this->returnError(401,"not found");
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return $this->returnError(401, "not found");
         }
-        if ($comment->user['id']!=Auth::id()){
-           return $this->returnError(401,"error");
+        if ($comment->user['id'] != Auth::id()) {
+            return $this->returnError(401, "error");
         }
         $comment->delete();
         return $this->returnSuccessMessage("Successfully");
     }
 
-    public function update(Request $request, int  $id): JsonResponse
+    public function update(Request $request, int $id): JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'text' => 'required|string',
@@ -67,80 +73,147 @@ class CommentController extends Controller
         if ($validator->fails()) {
             return $this->returnError(401, $validator->errors());
         }
-        $comment=Comment::find($id);
-        if (!$comment){
-            return $this->returnError(401,"not found");
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return $this->returnError(401, "not found");
         }
-        if ($comment->user['id']!=Auth::id()){
-            return $this->returnError(401,"error");
+        if ($comment->user['id'] != Auth::id()) {
+            return $this->returnError(401, "error");
         }
-        $comment['text']=$request->all()['text'];
-        $comment['edit']=true;
+        $comment['text'] = $request->all()['text'];
+        $comment['edit'] = true;
         $comment->save();
         return $this->returnSuccessMessage("Successfully");
     }
-    public function timeAgo($time_ago): string
+
+    public function timeAgo($time_ago, $lagn = "en"): string
     {
         $time_ago = strtotime($time_ago);
-        $cur_time   = time();
-        $time_elapsed   = $cur_time - $time_ago;
-        $seconds    = $time_elapsed ;
-        $minutes    = round($time_elapsed / 60 );
-        $hours      = round($time_elapsed / 3600);
-        $days       = round($time_elapsed / 86400 );
-        $weeks      = round($time_elapsed / 604800);
-        $months     = round($time_elapsed / 2600640 );
-        $years      = round($time_elapsed / 31207680 );
+        $cur_time = time();
+        $time_elapsed = $cur_time - $time_ago;
+        $seconds = $time_elapsed;
+        $minutes = round($time_elapsed / 60);
+        $hours = round($time_elapsed / 3600);
+        $days = round($time_elapsed / 86400);
+        $weeks = round($time_elapsed / 604800);
+        $months = round($time_elapsed / 2600640);
+        $years = round($time_elapsed / 31207680);
         // Seconds
-        if($seconds <= 60){
-            return "just now";
-        }
-        //Minutes
-        else if($minutes <=60){
-            if($minutes==1){
-                return "one minute ago";
+        if ($seconds <= 60) {
+            if ($lagn=="ar"){
+                return 'الأن';
             }
-            else{
+            return "just now";
+        } //Minutes
+        else if ($minutes <= 60) {
+            if ($minutes == 1) {
+                if ($lagn=="ar"){
+                    return 'منذ دقيقة';
+                }
+                return "one minute ago";
+            }elseif ($minutes==2){
+                if ($lagn=="ar"){
+                    return 'منذ دقيقتين';
+                }
                 return "$minutes minutes ago";
             }
-        }
-        //Hours
-        else if($hours <=24){
-            if($hours==1){
+            else {
+                if ($lagn=="ar"){
+                    return 'منذ '.$minutes.' دقائق';
+                }
+                return "$minutes minutes ago";
+            }
+        } //Hours
+        else if ($hours <= 24) {
+            if ($hours == 1) {
+                if ($lagn=="ar"){
+                    return 'منذ ساعة';
+                }
                 return "an hour ago";
-            }else{
+            }elseif ($hours==2){
+                if ($lagn=="ar"){
+                    return 'منذ ساعتين';
+                }
                 return "$hours hrs ago";
             }
-        }
-        //Days
-        else if($days <= 7){
-            if($days==1){
+            else {
+                if ($lagn=="ar"){
+                    return 'منذ '.$hours.' ساعات';
+                }
+                return "$hours hrs ago";
+            }
+        } //Days
+        else if ($days <= 7) {
+            if ($days == 1) {
+                if ($lagn=="ar"){
+                    return 'منذ يوم';
+                }
                 return "yesterday";
-            }else{
+            }elseif($days==2) {
+                if ($lagn == "ar") {
+                    return 'منذ يومين';
+                }
                 return "$days days ago";
             }
-        }
-        //Weeks
-        else if($weeks <= 4.3){
-            if($weeks==1){
+            else {
+                if ($lagn=="ar"){
+                    return 'منذ '.$days.' ايام';
+                }
+                return "$days days ago";
+            }
+        } //Weeks
+        else if ($weeks <= 4.3) {
+            if ($weeks == 1) {
+                if ($lagn=="ar"){
+                    return 'منذ اسبوع';
+                }
                 return "a week ago";
-            }else{
+            }elseif ($weeks==2){
+                if ($lagn=="ar"){
+                    return 'منذ اسبوعين';
+                }
                 return "$weeks weeks ago";
             }
-        }
-        //Months
-        else if($months <=12){
-            if($months==1){
+            else {
+                if ($lagn=="ar"){
+                    return 'منذ '.$weeks.' اسابيع';
+                }
+                return "$weeks weeks ago";
+            }
+        } //Months
+        else if ($months <= 12) {
+            if ($months == 1) {
+                if ($lagn=="ar"){
+                    return 'منذ شهر';
+                }
                 return "a month ago";
-            }else{
+            }elseif ($months==2){
+                if ($lagn=="ar"){
+                    return 'منذ شهرين';
+                }
+                return "$months months ago";
+            } else {
+                if ($lagn=="ar"){
+                    return 'منذ '.$months.' شهور';
+                }
                 return "$months months ago";
             }
-        }
-        //Years
-        else{
-            if($years==1){
+        } //Years
+        else {
+            if ($years == 1) {
+                if ($lagn=="ar"){
+                    return 'منذ سنة';
+                }
                 return "one year ago";
-            }else{
+            }elseif ($years==2){
+                if ($lagn=="ar"){
+                    return 'منذ سنتين';
+                }
+                return "$years years ago";
+            } else {
+                if ($lagn=="ar"){
+                    return 'منذ '.$years.'سنين';
+                }
                 return "$years years ago";
             }
         }
